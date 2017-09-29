c64d36b54b647b8184ab1e3ca8982e12a2c201d8
Martijn Slob <martijn.slob@algebrakit.nl>
2017-09-29 21:09:56 +0200

    simplified running single randomised exercise

--- generate_php/generate_exercise.php
+++ generate_php/generate_exercise.php
@@ -1,13 +1,23 @@
 <!DOCTYPE html>
+<!--
+This is a demo of how to run a randomized exercise from AlgebraKiT's CMS.
+- Server side (php):
+   * the number of levels for the exercise is obtained via  /exercise-info
+   * a session is created for the exercise using the highest level
+   * the resulting session id is used to set the attribute of <akit-exercise>
+- Client side (javascript)
+   * the widget is created by AlgebraKIT.injectWidgets()
+-->
 <?php
-    $host      = 'http://algebrakit.eu';
-    $appId     = '<your-app-id>';
-    $appSecret = '<your-app-password>';
-    $publishId = null;
+    $appId     =  '<your-app-id>';
+    $appSecret =  '<your-app-password';
+    $publishId =  '<publish-id-or-null>';
+    $courseId  =  '<id-of-course-containing-the-exercise>';
+    $exerciseId = '<id-of-the-randomized-exercise>';
     
     function akitPost($endpoint, $data) {
-        global $host, $appId, $appSecret;
-        $url  = $host.$endpoint;
+        global $appId, $appSecret;
+        $url  = 'https://algebrakit.eu'.$endpoint;
         $data['appId']     = $appId;
         $data['appSecret'] = $appSecret;
 
@@ -24,29 +34,21 @@
 
     //makes the call to AlgebraKiT's webservice to retrieve a session id from
     //an exercise specification
-    function getSessionId($exerciseSpec) {
-        $data = array(
-            'exerciseSpec' => $exerciseSpec
-        );
-        return akitPost('/session/create/spec', $data);
-    }
-    
-    function generateExercise($exerciseId, $nr, $level) {
+    function getSessionId($exId, $level) {
         global $courseId, $publishId;
         $data = array(
-            'exerciseId' => $exerciseId,
+            'exerciseId' => $exId,
             'courseId'   => $courseId,
             'publishId'  => $publishId,
-            'nr'         => $nr,
             'level'      => $level
         );
-        return akitPost('/exercise/generate/cms', $data);
+        return akitPost('/session/create/cms', $data);
     }
     
-    function getExerciseInfo($exerciseId) {
+    function getExerciseInfo($exId) {
         global $courseId, $publishId;
         $data = array(
-            'exerciseId' => $exerciseId,
+            'exerciseId' => $exId,
             'courseId'   => $courseId,
             'publishId'  => $publishId
         );
@@ -67,31 +69,12 @@
    </head>
    <body>
         <?php
-            $exInfo = getExerciseInfo('f3b7ea3a-240d-4de9-923a-c6d11f38c72d');
-            echo print_r($exInfo, true);
-            for($ii=0; $ii<$exInfo['numberOfLevels']; $ii++) {
-                $lev = $ii+1;
-                echo "<h3>Exercises of level $lev</h3>";
-                $exArr = generateExercise('f3b7ea3a-240d-4de9-923a-c6d11f38c72d', 5, $ii);
-                for($jj=0; $jj<count($exArr); $jj++) {
-                    $elm = $exArr[$jj]; 
-                    if($jj==0) {
-                        $resp = getSessionId($elm->exerciseSpec);
-                        $sessionId = $resp['sessionId'];
-                        $appId = $resp['appId'];
-                        echo "<akit-exercise session-id='$sessionId' app-id='$appId'></akit-exercise>";
-                    } else {
-                        $instruction = $elm->view->instruction[0]->content;
-                        $assignment = $elm->view->assignment[0]->content;
-                        echo "<div class='exercise'>";
-                        echo "  <div class='instruction'>$instruction</div>";
-                        echo "  <div class='assignment'>$assignment</div>";
-                    }
-                    echo "</div>";
-                }
-                
-            }
-
+            $exInfo = getExerciseInfo($exerciseId);
+            echo print_r($resp, true);
+            $lev = $exInfo['numberOfLevels'];
+            echo "<h3>Exercises of level $lev</h3>";
+            $sessionId = $resp['sessionId'];
+            echo "<akit-exercise session-id='$sessionId' app-id='$appId' start-active></akit-exercise>";
         ?>
        
         <!-- formula editor requires jquery -->
@@ -110,16 +93,9 @@
                 }
             };
         </script>
-        <?php echo "<script src='".$host."/akit-widgets.min.js'></script>";?>
-
+        <script src='https://algebrakit.eu/akit-widgets.min.js'></script>
         <script>
-            //trigger Katex to render all latex formulas
-            renderMathInElement(document.body, {
-            delimiters: [
-                {left: "$$", right: "$$", display: true},
-                {left: "$", right: "$", display: false}
-            ]});
-            AlgebraKIT.injectWidgets();
+             AlgebraKIT.injectWidgets();
         </script>
    </body>
 </home>
